28b1a242f2a786fe38bbc804a3f17dba7ee01a74
M4RC086 <marcomeulic@gmail.com>
2026-08-25 16:37:59 +0200

    add loops to only create the necesary folders and minor changes

--- quickclean.sh
+++ quickclean.sh
@@ -4,9 +4,9 @@ if [[ $1 == help ]] || [[ $1 == -h ]]  || [[ $1 == --help ]]; then
     echo "Usage: quickclean [folder]" 
 fi
 
-folder=$1
+FOLDER=$1
 if [[ -z $1 ]]; then
-    folder=.
+    FOLDER=.
 fi
 
 
@@ -14,25 +14,46 @@ TEXT=(doc docx eml msg odt pages rtf tex txt wpd)
 AUDIO=(mp3 mid ogg wav wma aif m3u m4a flac)
 VIDEO=(3gp asf avi flv m4v mov mp4 mpg swf ts vob wmv)
 IMAGE=(bmp dcm dds djvu gif heic jpg jpeg png psd tga tif jpp sktz jxl pxd)
-EXECUTABLES=(apk app bat bin cmd com exe ipa jar run sh)
+EXECUTABLE=(apk app bat bin cmd com exe ipa jar run sh)
 CODE=(appx c class cpp cs html css js h java kt lua m md pl py sb3 sln swift unity vb vcxproj xcodeproj yml)
 COMPRESSED=(7z cbr deb gz pkg rar rpm tar.gz xapk zip zipx)
-FILE_TYPES=(TEXT AUDIO VIDEO IMAGE EXEC COMPRESSED)
 
-# Create the folders to orgaize the files
-for c_folder in ${FILE_TYPES[@]}; do
-    mkdir $folder/${c_folder}
+FILE_TYPES=(TEXT AUDIO VIDEO IMAGE EXECUTABLE CODE COMPRESSED)
+USED_FILE_TYPES=()
+
+
+
+for type in ${FILE_TYPES[@]}; do
+
+    for file in $FOLDER/*; do    
+        declare -n c_type=$type 
+        for ext in ${c_type[@]}; do
+            if [[ $file == *$ext ]]; then
+
+                USED_FILE_TYPES+=($type)
+                break
+            fi
+        done
+    done
+    
+done
+
+# Create the folders to organise the files
+for c_folder in ${USED_FILE_TYPES[@]}; do
+    if [[ ! (-d $FOLDER/$c_folder) ]]; then
+        mkdir $FOLDER/${c_folder}
+    fi
 done
 
 
 # Move files into folders
-for file in $folder/*; do
-    for type in ${FILE_TYPES[@]}; do
+for type in ${USED_FILE_TYPES[@]}; do
+    for file in $FOLDER/*; do
         declare -n c_type=$type 
     
         for ext in ${c_type[@]}; do
             if [[ $file == *$ext ]]; then
-                mv $file $folder/$type
+                mv $file $FOLDER/$type/
                 break
             fi
         done
