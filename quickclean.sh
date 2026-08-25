6b65798b70e21cfe356099bcc1983123974d21e1
M4RC086 <marcomeulic@gmail.com>
2026-08-26 00:10:09 +0200

    add extensions and types

--- quickclean.sh
+++ quickclean.sh
@@ -11,14 +11,15 @@ fi
 
 
 TEXT=(doc docx eml msg odt pages rtf tex txt wpd)
-AUDIO=(mp3 mid ogg wav wma aif m3u m4a flac)
-VIDEO=(3gp asf avi flv m4v mov mp4 mpg swf ts vob wmv)
-IMAGE=(bmp dcm dds djvu gif heic jpg jpeg png psd tga tif jpp sktz jxl pxd)
-EXECUTABLE=(apk app bat bin cmd com exe ipa jar run sh)
-CODE=(appx c class cpp cs html css js h java kt lua m md pl py sb3 sln swift unity vb vcxproj xcodeproj yml)
-COMPRESSED=(7z cbr deb gz pkg rar rpm tar.gz xapk zip zipx)
+AUDIO=(mp3 mid ogg wav wma aif m3u m4a flac opus)
+VIDEOS=(3gp asf avi flv m4v mov mp4 mpg swf ts vob wmv mkv)
+IMAGES=(bmp dcm dds djvu gif heic jpg jpeg png psd tga tif jpp sktz jxl pxd tiff)
+EXECUTABLES=(apk app bat bin cmd com exe ipa jar run sh ex)
+CODE=(appx c class cpp cs html css js h java kt lua m md pl py jsx sb3 sln swift unity vb vcxproj xcodeproj yml rb php h hpp r sql php rs)
+COMPRESSED=(7z cbr deb gz pkg rar rpm tar.gz xapk zip zipx tar)
+PDFS=(pdf)
 
-FILE_TYPES=(TEXT AUDIO VIDEO IMAGE EXECUTABLE CODE COMPRESSED)
+FILE_TYPES=(TEXT AUDIO VIDEOS IMAGES EXECUTABLES CODE COMPRESSED PDFS)
 USED_FILE_TYPES=()
 
 
